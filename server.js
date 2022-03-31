74480bcaf2559d9501649c2d3f433580ad4b945e
Pablo Mendez <pablomendev@gmail.com>
2022-03-31 15:10:33 -0300

    agregado verbo http al que responden los puertos

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const contenedorDeProductos = new Contenedor('productos.txt')
 const PORT = process.env.PORT || 8080
 
 app.get('/', (_, res) => {
-	res.send('<h2>Endopoints disponibles</h2><h4>/productos</h4><h4>/productoRandom</h4>')
+	res.send('<h2>Endopoints disponibles (GET)</h2><h4>/productos</h4><h4>/productoRandom</h4>')
 })
 
 app.get('/productos', async (_, res) => {
