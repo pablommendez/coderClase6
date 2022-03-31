7add2ae5ff7d48f9d0c5a35d52bc7231827ac94c
Pablo Mendez <pablomendev@gmail.com>
2022-03-31 15:08:06 -0300

    Agregado endpoint /

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express')
-const{Contenedor} = require('./models/Contenedor')
+const { Contenedor } = require('./models/Contenedor')
 
 const app = express()
 const contenedorDeProductos = new Contenedor('productos.txt')
 const PORT = process.env.PORT || 8080
-app.get('/productos',async (req, res) => {
-    res.json(await contenedorDeProductos.getAll())
+
+app.get('/', (_, res) => {
+	res.send('<h2>Endopoints disponibles</h2><h4>/productos</h4><h4>/productoRandom</h4>')
 })
 
-app.get('/productoRandom', async (req, res) => {
-    res.json(await contenedorDeProductos.geProductoRandom())
+app.get('/productos', async (_, res) => {
+	res.json(await contenedorDeProductos.getAll())
+})
+
+app.get('/productoRandom', async (_, res) => {
+	res.json(await contenedorDeProductos.geProductoRandom())
 })
 
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en puerto ${PORT}`);
+	console.log(`Servidor escuchando en puerto ${PORT}`);
 })
